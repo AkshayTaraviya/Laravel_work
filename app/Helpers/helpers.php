cb2d7279778b13e427f5768c7c45924122ed9e15
AkshayTaraviya <akshaytaraviya5367@gmail.com>
2024-02-19 15:28:22 +0530

    file change

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,16 +1,9 @@
 <?php
 
-if (!function_exists('lcfirst')) {
-    function lcfirst($str)
-    {
-        return strtolower(substr($str, 0, 1)) . substr($str, 1);
-    }
-}
-
-
 if (!function_exists('ucfirst')) {
     function ucfirst($str)
     {
         return strtolower(substr($str, 0, 1)) . substr($str, 1);
     }
 }
+
