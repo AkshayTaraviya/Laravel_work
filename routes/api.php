cbf9986012ea0eddeb27c77cb442dcafea5036ca
AkshayTaraviya <akshaytaraviya5367@gmail.com>
2024-02-20 19:17:53 +0530

    add changes

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 //Public Routes
 Route::get('/users',[UserController::class, 'index']);
 Route::get('/users/{id}',[UserController::class, 'show']);
 Route::post('/users',[UserController::class,'store']);
 Route::put('/users/{id}',[UserController::class,'update']);
 Route::delete('/users/{id}',[UserController::class,'destroy']);
+
+// Protected Routes
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
