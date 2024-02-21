65860b9c87f667fe1e48aa585ede9794a59a30d7
AkshayTaraviya <akshaytaraviya5367@gmail.com>
2024-02-21 15:38:26 +0530

    add message

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 // Route::put('/users/{id}',[UserController::class,'update']);
 // Route::delete('/users/{id}',[UserController::class,'destroy']);
 
+// Route::resource('users', UserController::class);
+
    Route::post('/register', [CustomAuthController::class, 'register']);
    Route::post('/login', [CustomAuthController::class, 'login']);
 
